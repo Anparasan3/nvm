dcff5c1f8bddc8b49260acb5e128304b56094ea3
Anparasan3 <anparasantamilarasan@gmail.com>
2023-04-21 22:32:33 +0530

    created server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const port = 9000;
+const app = express();
+
+app.use("/", (req, res) => {
+    const file = path.join(__dirname, "nvm.sh");
+    // const file = path.join(__dirname, "index.html");
+    // res.sendFile(file)
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(file)
+});
+
+app.listen(9000, () => {
+    console.log(`Starting Server on Port ${port}`);
+});
